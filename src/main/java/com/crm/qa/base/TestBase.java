0257b079800b8d70e525655e74d935b09dc2f7e5
harshadaP97 <hpatil10697@gmail.com>
2025-05-25 21:35:06 +0100

    Chromedriver generic chnages

--- src/main/java/com/crm/qa/base/TestBase.java
+++ src/main/java/com/crm/qa/base/TestBase.java
@@ -1,6 +1,5 @@
-/* Harshada Patil
- * 
- */
+/* Harshada Patil */
+
 package com.crm.qa.base;
 
 import java.io.FileInputStream;
@@ -11,71 +10,74 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 import com.crm.qa.util.TestUtil;
 
+
 public class TestBase {
-	
-	public static WebDriver driver;
-	public static Properties prop;
 
-	public TestBase()   
-	{
-		try {
-			prop = new Properties();
-			FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "\\src\\main\\java\\com\\crm\\qa\\config\\config.properties");
-			prop.load(ip);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
-	
-	public static void init()
-	{
-		String bName = prop.getProperty("browser");
-		String driverPath = "E:\\HarshadaStudy\\Selenium\\chromedriver.exe";
+    public static WebDriver driver;
+    public static Properties prop;
 
-		if(bName.equals("chrome"))
-		{
-			System.out.println(driverPath);
-			System.setProperty("webdriver.chrome.driver",driverPath);
-			
-			driver = new ChromeDriver();
-		}
-		else if(bName.equals("FF"))
-		{
-			//System.setProperty("webdriver.gecho.driver","/Users/naveenkhunteta/Documents/SeleniumServer/geckodriver");
-			driver= new FirefoxDriver();
-		}
-		
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		driver.manage().timeouts().pageLoadTimeout(TestUtil.page_load_timeout,TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(TestUtil.implicit_wait,TimeUnit.SECONDS);
-		
-		driver.get(prop.getProperty("url"));
-	}
-	
-	
-
-@AfterMethod
-public void tearDown(ITestResult result) {
-    if (ITestResult.FAILURE == result.getStatus()) {
+    public TestBase() {
         try {
-            TestUtil.takeScreenshotAtEndOfTest();
-            System.out.println("Screenshot taken on failure.");
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(
+                System.getProperty("user.dir") + "\\src\\main\\java\\com\\crm\\qa\\config\\config.properties"
+            );
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-    
-    driver.close();
-}
 
+    public static void init() {
+        String bName = prop.getProperty("browser");
+
+        if (bName.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new"); // Headless mode for Jenkins
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--remote-allow-origins=*");
+
+            driver = new ChromeDriver(options);
+        } else if (bName.equalsIgnoreCase("FF")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver(); // You can add FirefoxOptions similarly
+        }
+
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(TestUtil.page_load_timeout, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(TestUtil.implicit_wait, TimeUnit.SECONDS);
+
+        driver.get(prop.getProperty("url"));
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) {
+        if (ITestResult.FAILURE == result.getStatus()) {
+            try {
+                TestUtil.takeScreenshotAtEndOfTest();
+                System.out.println("Screenshot taken on failure.");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
